408ca9217fb7a4cecb15c9827364854d0379b0e7
OfelyaMuradyan <138681012+OfelyaMuradyan@users.noreply.github.com>
2023-09-17 12:27:23 +0400

    Add files via upload

---
+++ 18.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+        int A = 0;
+        int B = 0;
+        int C = 0;
+
+        for(A = 0; A <= 1; A++)
+        {
+                for(B = 0; B <= 1; B++)
+                {
+                        C = A || B && !(B || A);
+                        printf("%d %d is %d\n", A , B, C);
+                }
+        }
+}
